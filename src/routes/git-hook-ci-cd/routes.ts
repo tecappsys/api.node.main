f03c08aa8b4e99d73507b7bc94e867083b364c4f
Daniel <danieldiazmc@gmail.com>
2025-03-03 21:39:33 -0400

    update routes get by post

--- src/routes/git-hook-ci-cd/routes.ts
+++ src/routes/git-hook-ci-cd/routes.ts
@@ -4,8 +4,8 @@ import { UpdateNodeMain, UpdateRepos } from '../../controllers/git-hook-ci-cd/Co
 
 const routes = Router();
 
-routes.get('/',VerifyGitHook,UpdateRepos);
-routes.get('/node-main',VerifyGitHook,UpdateNodeMain);
+routes.post('/',VerifyGitHook,UpdateRepos);
+routes.post('/node-main',VerifyGitHook,UpdateNodeMain);
 
 
 export default routes;
